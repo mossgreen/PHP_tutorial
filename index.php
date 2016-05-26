3dd8cb9a99149332c240025549383db353aea6d4
Moss Green <gufeifei@outlook.com>
2016-05-27 10:20:48 +1200

    added one item

--- index.php
+++ index.php
@@ -44,7 +44,18 @@
 		</div>	
 		<!-- main content -->
 		<div class="col-md-8">
-			main body
+			<div class="row">
+				<h2 class="text-center">
+					Featured Products
+				</h2>
+				<div class="col-md-3">
+					<h4>Levis Jeans</h4>
+					<img src="images/products/men4.png" alt="Levis Jeans" />
+					<p class="list-price text-danger">List Price: <s>$54.99</s></p>
+					<p class="price">Our Price:$199.99</p>
+					<button class="btn btn-sm btn-success" type="button" data-toggle="model" data-target="#details-1"> Details</button>
+				</div>
+			</div>
 		</div>
 
 		<!-- right side bar -->

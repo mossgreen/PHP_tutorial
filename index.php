0e873ebbea688184792697edaa2193c4070a6ebc
Moss Green <gufeifei@outlook.com>
2016-05-26 22:37:10 +1200

    added a js to move pic while scrolling

--- index.php
+++ index.php
@@ -40,5 +40,26 @@
 	<p>
 		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate perferendis obcaecati iste, culpa est eum, facere dolore nam mollitia, repudiandae eaque consequuntur quibusdam et laudantium dolorum, illo possimus ab incidunt.
 	</p>
+
+	<script type="text/javascript">
+		
+	jQuery(window).scroll(function(){
+		var vscroll = jQuery(this).scrollTop();
+		jQuery('#logotext').css({
+			"tansform" : "translate(0px, "+vscroll/2+"px)"
+		});
+
+				var vscroll = jQuery(this).scrollTop();
+		jQuery('#back-flower').css({
+			"tansform" : "translate("+vscroll/5+"px, -"+vscroll/12+"px)"
+		});
+
+				var vscroll = jQuery(this).scrollTop();
+		jQuery('#fore-flower').css({
+			"tansform" : "translate(0px, -"+vscroll/2+"px)"
+		});
+	});
+
+	</script>
 </body>
 </html>

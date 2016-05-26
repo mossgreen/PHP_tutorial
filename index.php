38913caeb2557698351219f4601fde18bc51efde
Moss Green <gufeifei@outlook.com>
2016-05-27 10:51:22 +1200

    added footer

--- index.php
+++ index.php
@@ -126,6 +126,8 @@
 		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate perferendis obcaecati iste, culpa est eum, facere dolore nam mollitia, repudiandae eaque consequuntur quibusdam et laudantium dolorum, illo possimus ab incidunt.
 	</p>
 
+	<footer class="text-center" id="footer">&copy; CopyRight 2013-2015 moss'shop</footer>
+
 	<script type="text/javascript">
 		
 	jQuery(window).scroll(function(){

89e4deae5c591dbde1b153528ea9eee656695a97
Moss Green <gufeifei@outlook.com>
2016-05-28 10:47:59 +1200

    tested.
    separated head as php file

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>This is title</title>
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
-	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-	<script  src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-	<script src="js/bootstrap.min.js" ></script>
-</head>
-<body>
+<?php include 'includes/head.php'; ?>
 	<!-- top nav ba -->
 	<nav class="navbar navbar-default navbar-fixed-top">
 		<div class="contianer">

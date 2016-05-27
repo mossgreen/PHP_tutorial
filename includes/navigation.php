076d19ee96c19c782f09a2aecfb90dd782badcf5
Moss Green <gufeifei@outlook.com>
2016-05-28 11:52:50 +1200

    bug fixed.
    category name

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,24 +1,24 @@
 <?php 
-	$sql = "SELECT * FROM categories WHERE  parent = 0";
-	$pquery = $db -> query($sql);
+$sql = "SELECT * FROM categories WHERE  parent = 0";
+$pquery = $db -> query($sql);
 
 
- ?>
+?>
 
 
 <nav class="navbar navbar-default navbar-fixed-top">
-		<div class="contianer">
+	<div class="contianer">
 		<a href="index.php" class="navbar-brand">moss's shop</a>
-			<ul class="nav navbar-nav">
+		<ul class="nav navbar-nav">
 
 			<?php while($parent = mysqli_fetch_assoc($pquery)): ?>
+				<?php $parent_id = $parent['id']; ?>
 
 
 
-
-
+				<!-- menu items -->
 				<li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Men<span class="caret"></span></a>
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo $parent['category']; ?><span class="caret"></span></a>
 					<ul class="dropdown-menu" role="menu">
 						<li><a href="#">Shirts</a></li>
 						<li><a href="#">Pants</a></li>
@@ -28,7 +28,7 @@
 				</li>
 				
 
-				<?php endwhile; ?>
-			</ul>
-		</div>
-	</nav>
+			<?php endwhile; ?>
+		</ul>
+	</div>
+</nav>

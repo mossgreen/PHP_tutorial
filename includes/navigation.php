f2ab3a02564197fcac79c24e288eaf23a41cbc06
Moss Green <gufeifei@outlook.com>
2016-05-28 10:50:50 +1200

    tested.
    navigation to php file

---
+++ includes/navigation.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="contianer">
+		<a href="index.php" class="navbar-brand">moss's shop</a>
+			<ul class="nav navbar-nav">
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Men<span class="caret"></span></a>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="#">Shirts</a></li>
+						<li><a href="#">Pants</a></li>
+						<li><a href="#">Shoes</a></li>
+						<li><a href="#">Accessories</a></li>
+		
+					</ul>
+				</li>
+				<li class="dropdown"></li>
+				<li class="dropdown"></li>
+			</ul>
+		</div>
+	</nav>
--- index.php
+++ index.php
@@ -1,24 +1,11 @@
-<?php include 'includes/head.php'; ?>
+<?php 
+	include 'includes/head.php'; 
+	include 'includes/navigation.php';
+
+
+?>
 	<!-- top nav ba -->
-	<nav class="navbar navbar-default navbar-fixed-top">
-		<div class="contianer">
-		<a href="index.php" class="navbar-brand">moss's shop</a>
-			<ul class="nav navbar-nav">
-				<li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Men<span class="caret"></span></a>
-					<ul class="dropdown-menu" role="menu">
-						<li><a href="#">Shirts</a></li>
-						<li><a href="#">Pants</a></li>
-						<li><a href="#">Shoes</a></li>
-						<li><a href="#">Accessories</a></li>
-		
-					</ul>
-				</li>
-				<li class="dropdown"></li>
-				<li class="dropdown"></li>
-			</ul>
-		</div>
-	</nav>
+	
 
 	<!-- Header -->
 	<div id="headerWrapper">

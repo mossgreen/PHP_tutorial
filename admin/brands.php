604f4574afbf6e3e189031f849a5e7fcb86ee745
Moss Green <gufeifei@outlook.com>
2016-05-30 12:35:12 +1200

    error fixed.

--- admin/brands.php
+++ admin/brands.php
@@ -4,8 +4,8 @@ include 'includes/head.php';
 include 'includes/navigation.php';
 
 //get brands from db
-$sql = "SELECT * FROM brand ORDER BY brands";
-$result = $db -> query($sql);
+$sql = "SELECT * FROM brand ORDER BY brand";
+$results = $db -> query($sql);
 
 ?>
 <h2 class="text-center">Brands</h2>
@@ -18,7 +18,7 @@ $result = $db -> query($sql);
 	</thead>
 	<tbody>
 
-	<?php while($brand = msqli_fetch_assoc($result)): ?>
+	<?php while($brand = mysqli_fetch_assoc($results)): ?>
 		<tr>
 			<td><a href="brands.php?edit=1" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-pencil"></span> </a></td>
 			<td><?php echo $brand['brand']; ?></td>

a85db6874440a002ee702aa09324036177326862
Moss Green <gufeifei@outlook.com>
2016-06-01 09:55:16 +1200

    bug: button on index is not working

--- core/init.php
+++ core/init.php
@@ -5,7 +5,7 @@ if(mysqli_connect_errno()){
 	die();
 }
 
-require_once '../config.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/tutorial/config.php';
 require_once BASEURL."helpers/helpers.php";
 
 

7976844a50bfa790d237e38eaf5c05090c85f6a1
Moss Green <gufeifei@outlook.com>
2016-06-02 10:48:56 +1200

    tested
    http://localhost/tutorial/admin/categories.php

---
+++ admin/categories.php
@@ -0,0 +1,12 @@
+<?php 
+
+require_once $_SERVER['DOCUMENT_ROOT'].'/tutorial/core/init.php';
+include 'includes/head.php';
+include 'includes/navigation.php';
+
+ ?>
+
+ <?php 
+
+
+ include 'includes/footer.php' ;?>

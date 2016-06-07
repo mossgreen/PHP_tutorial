212c2f6313f32f0cb2c43fa46e309b9f825a2fe2
Moss Green <gufeifei@outlook.com>
2016-06-07 17:30:45 +1200

    bug fixed.
    1. need “;” at the end of any sentence

--- admin/products.php
+++ admin/products.php
@@ -1,14 +1,13 @@
 <?php 
 require_once $_SERVER['DOCUMENT_ROOT'].'/tutorial/core/init.php';;
 include 'includes/head.php';
-include 'includes/navigation.php'
-$sql = "SELECT * FROM products WHERE deleted = 0"
+include 'includes/navigation.php';
+$sql = "SELECT * FROM products WHERE deleted = 0";
 $presults = $db -> query($sql);
 ?>
 
 <h2 class="text-center">Products</h2>
 <hr>
-
 <table class="table table-bordered table-condensed table-striped">
 	<thead>
 		<th></th>
@@ -19,7 +18,7 @@ $presults = $db -> query($sql);
 		<th>Sold</th>
 	</thead>
 	<tbody>
-		<?php while($product = mysqi_fetch_assoc($presults)): ?>
+		<?php while($product = mysqli_fetch_assoc($presults)): ?>
 
 			<tr>
 				<td>

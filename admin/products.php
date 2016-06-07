284b8fc034935b98d900cd104ce66cf6f93dec27
Moss Green <gufeifei@outlook.com>
2016-06-07 19:05:09 +1200

    error
    Notice: Undefined index: categories in
    /Applications/XAMPP/xamppfiles/htdocs/tutorial/admin/products.php on
    line 29

--- admin/products.php
+++ admin/products.php
@@ -7,7 +7,7 @@ $presults = $db -> query($sql);
 if(isset($_GET['featured'])) {
 	$id = (int)$_GET['id'];
 	$featured = (int)$_GET['featured'];
-	$featuredSql = "UPDATE products SET featured = '$featured' WHERE id=$id ";
+	$featuredSql = "UPDATE products SET featured = $featured WHERE id=$id ";
 	$db -> query($featuredSql);
 	header('Location: products.php');
 }
@@ -25,27 +25,37 @@ if(isset($_GET['featured'])) {
 		<th>Sold</th>
 	</thead>
 	<tbody>
-		<?php while($product = mysqli_fetch_assoc($presults)): ?>
+		<?php while($product = mysqli_fetch_assoc($presults)): 
+			$childID = $product['categories'];
+			$catSql = "SELECT * FROM categories WHERE id=$childID";
+			$result = $db -> query($catSql);
+			$child = mysqli_fetch_assoc($result);
+			$parentID = $child['parent'];
+			$pSql = "SELECT * FROM categories WHERE id= '$parentID' ";
+			$presult = $db -> query($pSql);
+			$parent = mysqli_fetch_assoc($presult);
+			$category = $parent['category'].'~'.$child['category'];
+		?>
 
-			<tr>
-				<td>
-					<a href="products.php?edit=<?=$product['id']?>" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-pencil"></span></a>
-					<a href="products.php?delete=<?=$product['id']?>" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-remove"></span></a>
-				</td>
-				<td><?=$product['title'];?></td>
-				<td><?=money($product['price']);?></td>
-				<td></td>
-				<td>
-					<a href="products.php?featured=<?=(($product['featured'] == 0)?'1':'0');?>&id=<?=$product['id'];?>" class="btn btn-xs btn-default ">
-						<span class="glyphicon glyphicon-<?=(($product['featured'] == 1)?'minus':'plus');?>">
-						</span>
-					</a>
-					&nbsp <?=(($product['featured'] == 1)?'Featured product':'');?>
-				</td>
-				<td></td>
-			</tr>
-		<?php endwhile; ?>
-	</tbody>
+		<tr>
+			<td>
+				<a href="products.php?edit=<?=$product['id']?>" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-pencil"></span></a>
+				<a href="products.php?delete=<?=$product['id']?>" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-remove"></span></a>
+			</td>
+			<td><?=$product['title'];?></td>
+			<td><?=money($product['price']);?></td>
+			<td><?=$category;?></td>
+			<td>
+				<a href="products.php?featured=<?=(($product['featured'] == 0)?'1':'0');?>&id=<?=$product['id'];?>" class="btn btn-xs btn-default ">
+					<span class="glyphicon glyphicon-<?=(($product['featured'] == 1)?'minus':'plus');?>">
+					</span>
+				</a>
+				&nbsp <?=(($product['featured'] == 1)?'Featured product':'');?>
+			</td>
+			<td></td>
+		</tr>
+	<?php endwhile; ?>
+</tbody>
 </table>
 
 

903367f80345dfdf06cf8388aad2701efb27d254
Moss Green <gufeifei@outlook.com>
2016-06-07 20:24:50 +1200

    error
    Fatal error: Cannot use isset() on the result of an expression (you can
    use "null !== expression" instead) in
    /Applications/XAMPP/xamppfiles/htdocs/tutorial/admin/products.php on
    line 19

--- admin/products.php
+++ admin/products.php
@@ -2,68 +2,92 @@
 require_once $_SERVER['DOCUMENT_ROOT'].'/tutorial/core/init.php';;
 include 'includes/head.php';
 include 'includes/navigation.php';
-$sql = "SELECT * FROM products WHERE deleted = 0";
-$presults = $db -> query($sql);
-if(isset($_GET['featured'])) {
-	$id = (int)$_GET['id'];
-	$featured = (int)$_GET['featured'];
-	$featuredSql = "UPDATE products SET featured = $featured WHERE id=$id ";
-	$db -> query($featuredSql);
-	header('Location: products.php');
-}
-?>
+if (isset($_GET['add'])) {
+	$brandQuery = $db -> query("SELECT * FROM brand");
 
-<h2 class="text-center">Products</h2>
-<a href="products.php?add=1" class="btn btn-success pull-right" id="add-product-btn">
-	Add Product
-</a>
-<div class="clearfix"></div>
-<hr>
-<table class="table table-bordered table-condensed table-striped">
-	<thead>
-		<th></th>
-		<th>Product</th>
-		<th>Price</th>
-		<th>Categories</th>
-		<th>Featured</th>
-		<th>Sold</th>
-	</thead>
-	<tbody>
-		<?php while($product = mysqli_fetch_assoc($presults)): 
-		$childID = $product['categories'];
-		$catSql = "SELECT * FROM categories WHERE id=$childID";
-		$result = $db -> query($catSql);
-		$child = mysqli_fetch_assoc($result);
-		$parentID = $child['parent'];
-		$pSql = "SELECT * FROM categories WHERE id= '$parentID' ";
-		$presult = $db -> query($pSql);
-		$parent = mysqli_fetch_assoc($presult);
-		$category = $parent['category'].'~'.$child['category'];
+	?>
+	<h2 class="text-center">Add A New Product</h2>
+	<hr>
+	<form action="products.php?add=1" method="POST" enctype="multipart/form-data">
+		<div class="form-group col-md-3">
+			<label for="title">Title*:</label>
+			<input class="form-control" type="text" name="title" id="title" value="<?=((isset($_POST['title']))?sanitize($_POST['title']):'');?>">
+		</div>
+		<div class="form-group col-md-3">
+			<label for="brand">brand*:</label>
+			<select class="form-control" id="brand" name="brand">
+				<option value="" <?=((isset($_POST['brand'] && $_POST['brand'] == ''))?'':'');?>></option>
+				<?php while($brand = mysqli_fetch_assoc($brandQuery)): ?>
+					<option value="<?=$brand['id'];?>" <?=((isset($_POST['brand'] && $_POST['brand'] == $brand['id']))?' selected':'');?>><?=$brand['brand'];?></option>
+				<?php endwhile; ?>
+			</select>
+		</div>
+	</form>
+	<?php }else{
+
+
+		$sql = "SELECT * FROM products WHERE deleted = 0";
+		$presults = $db -> query($sql);
+		if(isset($_GET['featured'])) {
+			$id = (int)$_GET['id'];
+			$featured = (int)$_GET['featured'];
+			$featuredSql = "UPDATE products SET featured = $featured WHERE id=$id ";
+			$db -> query($featuredSql);
+			header('Location: products.php');
+		}
 		?>
-		<tr>
-			<td>
-				<a href="products.php?edit=<?=$product['id']?>" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-pencil"></span></a>
-				<a href="products.php?delete=<?=$product['id']?>" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-remove"></span></a>
-			</td>
-			<td><?=$product['title'];?></td>
-			<td><?=money($product['price']);?></td>
-			<td><?=$category;?></td>
-			<td>
-				<a href="products.php?featured=<?=(($product['featured'] == 0)?'1':'0');?>&id=<?=$product['id'];?>" class="btn btn-xs btn-default ">
-					<span class="glyphicon glyphicon-<?=(($product['featured'] == 1)?'minus':'plus');?>">
-					</span>
-				</a>
-				&nbsp <?=(($product['featured'] == 1)?'Featured product':'');?>
-			</td>
-			<td></td>
-		</tr>
-	<?php endwhile; ?>
-</tbody>
-</table>
+
+		<h2 class="text-center">Products</h2>
+		<a href="products.php?add=1" class="btn btn-success pull-right" id="add-product-btn">
+			Add Product
+		</a>
+		<div class="clearfix"></div>
+		<hr>
+		<table class="table table-bordered table-condensed table-striped">
+			<thead>
+				<th></th>
+				<th>Product</th>
+				<th>Price</th>
+				<th>Categories</th>
+				<th>Featured</th>
+				<th>Sold</th>
+			</thead>
+			<tbody>
+				<?php while($product = mysqli_fetch_assoc($presults)): 
+				$childID = $product['categories'];
+				$catSql = "SELECT * FROM categories WHERE id=$childID";
+				$result = $db -> query($catSql);
+				$child = mysqli_fetch_assoc($result);
+				$parentID = $child['parent'];
+				$pSql = "SELECT * FROM categories WHERE id= '$parentID' ";
+				$presult = $db -> query($pSql);
+				$parent = mysqli_fetch_assoc($presult);
+				$category = $parent['category'].'~'.$child['category'];
+				?>
+				<tr>
+					<td>
+						<a href="products.php?edit=<?=$product['id']?>" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-pencil"></span></a>
+						<a href="products.php?delete=<?=$product['id']?>" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-remove"></span></a>
+					</td>
+					<td><?=$product['title'];?></td>
+					<td><?=money($product['price']);?></td>
+					<td><?=$category;?></td>
+					<td>
+						<a href="products.php?featured=<?=(($product['featured'] == 0)?'1':'0');?>&id=<?=$product['id'];?>" class="btn btn-xs btn-default ">
+							<span class="glyphicon glyphicon-<?=(($product['featured'] == 1)?'minus':'plus');?>">
+							</span>
+						</a>
+						&nbsp <?=(($product['featured'] == 1)?'Featured product':'');?>
+					</td>
+					<td></td>
+				</tr>
+			<?php endwhile; ?>
+		</tbody>
+	</table>
 
 
 
-<?php 
-include 'includes/footer.php';
+	<?php }
+	include 'includes/footer.php';
 
-?>
+	?>

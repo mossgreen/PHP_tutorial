8070228e0221081f56e4175cd6b4732a62d8e0e6
Moss Green <gufeifei@outlook.com>
2016-06-08 20:36:44 +1200

    bug fixed

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -6,7 +6,7 @@
 		function updateSizes(){
 			var sizeString = '';
 			for(var i =1; i <= 12; i++){
-				if(jQuery('#size'+i) != ''){
+				if(jQuery('#size'+i).val() != ''){
 					sizeString += jQuery('#size' + i).val() + ':' + jQuery('#qty' +1).val()+',';
 				}
 			}

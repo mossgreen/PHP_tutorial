09b2ab7b00dfd454e0c7e687c24f9f40d8ace697
Moss Green <gufeifei@outlook.com>
2016-06-10 23:06:11 +1200

    bug fixed.
    $sizeString -> $sizestring

--- includes/detailsmodal.php
+++ includes/detailsmodal.php
@@ -9,9 +9,9 @@ $brand_id = $product['brand'];
 $sql = "SELECT brand FROM brand WHERE id='$brand_id'";
 $brand_query = $db -> query($sql);
 $brand = mysqli_fetch_assoc($brand_query);
-var_dump($product);
+//var_dump($product);
 $sizestring = $product['sizes'];
-$sizestring = rtrim($sizeString,',');
+$sizestring = rtrim($sizestring,',');
 $size_array = explode(',',$sizestring);
 ?>
 

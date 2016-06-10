d76190df326020fe886337b8eb023c41a7a9bd50
Moss Green <gufeifei@outlook.com>
2016-06-11 10:50:21 +1200

    bug fixed with sql syntax
    there is no ‘,’, before WHERE

--- admin/products.php
+++ admin/products.php
@@ -22,10 +22,12 @@ if (isset($_GET['add']) || isset($_GET['edit'])) {
 		$edit_id = (int)$_GET['edit'];
 		$productResults = $db -> query("SELECT * FROM products WHERE id ='$edit_id'");
 		$product = mysqli_fetch_assoc($productResults);
+
+		//delete image action
 		if(isset($_GET['delete_image'])){
 			$image_url = $_SERVER['DOCUMENT_ROOT'].$product['image']; echo $image_url;
 			echo $image_url;
-			//unlink($image_url);   
+			unlink($image_url);   
 			$db -> query("UPDATE products SET image = '' WHERE id= '$edit_id' ");
 			header('Location: products.php?edit='.$edit_id);
 		}
@@ -43,254 +45,258 @@ if (isset($_GET['add']) || isset($_GET['edit'])) {
 		$saved_image = (($product['image'] != '')?$product['image']:'');
 		$dbpath = $saved_image;
 	}
+
 	if (!empty($sizes)) {
 		$sizeString = sanitize($sizes);
 		$sizeString = rtrim($sizeString,','); 
 		$sizesArray = explode(',',$sizeString);
 		$sArray = array();
 		$qArray = array();
+
 		foreach ($sizesArray as $ss ) {
 			$s = explode(':', $ss);
 			$sArray[] = $s[0];
-			$qArray[] = $s[1];		}
-		}else{ $sizesArray = array(); }
+			$qArray[] = $s[1];		
+		}
+	}else{ $sizesArray = array(); }
 
-		if ($_POST) {
-			$dbpath = '';
-			$errors = array();
-			$required = array('title', 'brand','price','parent','child','sizes');
-			foreach($required as $field){
-				if($_POST[$field] == ''){
-					$errors[] = 'All Fields With and Astrisk are required.';
-					break;
-				}
-			}
-
-			if(!empty($_FILES)){
-				var_dump($_FILES);
-				$photo = $_FILES['photo'];
-				$name = $photo['name'];
-				$nameArray = explode('.',$name);
-				$fileName = $nameArray[0];
-				$fileExt = $nameArray[1];
-				$mime = explode('/',$photo['type']);
-				$mimeType = $mime[0];
-				$mimeExt = $mime[1];
-				$tmpLoc = $photo['tmp_name'];
-				$fileSize = $photo['size'];
-				$allowed = array('png','jpg','jpeg','gif','PNG');
-				$uploadName = md5(microtime()).'.'.$fileExt;
-				$uploadPath = BASEURL.'/images/products/'.$uploadName;
-				$dbpath = '/tutorial/images/products/'.$uploadName;
-				if($mimeType != 'image'){
-					$errors[] = 'The file must be an image.';
-				}
-
-				if(!in_array($fileExt, $allowed)){
-					$errors[] = 'The file extension must be a png, jpg, jpeg, or gif.';
-				}
-				if($fileSize > 25000000){
-					$errors[] = 'The files size must be under 25 MB.';
-				}
-				if($fileExt != $mimeExt && ($mimeExt == 'jpeg' && $fileExt != 'jgp')){
-					$errors[] = 'File extension does not match the file.';
-				}
-			}
-			if(!empty($errors)){
-				echo display_errors($errors);
-			}else{
-				//upload file and insert into database
-				move_uploaded_file($tmpLoc, $uploadPath);
-				$insertSql = "INSERT INTO products (title,price,list_price,brand,categories,sizes,image,description) VALUES('$title','$price','$list_price', '$brand','$category', '$sizes','$dbpath', '$description')";
-
-				if(isset($_GET['edit'])){
-					$insertSql = "UPDATE products SET title = '$title', price = '$price' list_price = '$list_price',brand = '$brand', categories = '$category',sizes = '$sizes', image = '$dbpath', description = '$description' WHERE id='$edit_id'";
-				}
-				$db -> query($insertSql);
-				header('Location: products.php');
+	if ($_POST) {
+		// $dbpath = '';
+		$errors = array();
+		$required = array('title', 'brand','price','parent','child','sizes');
+		foreach($required as $field){
+			if($_POST[$field] == ''){
+				$errors[] = 'All Fields With and Astrisk are required.';
+				break;
 			}
 		}
-		?>
-		<h2 class="text-center"><?=((isset($_GET['edit']))?'Edit ':'Add A New ');?>Product</h2>
-		<hr>
-		<form action="products.php?<?=((isset($_GET['edit']))?'edit='.$edit_id:'add=1');?>" method="POST" enctype="multipart/form-data">
-			<div class="form-group col-md-3">
-				<label for="title">Title*:</label>
-				<input class="form-control" type="text" name="title" id="title" value="<?=$title;?>">
-			</div>
-			<div class="form-group col-md-3">
-				<label for="brand">brand*:</label>
-				<select class="form-control" id="brand" name="brand">
-					<option value="" <?=(($brand == '')?' selected':'');?>></option>
-					<?php while($bresult = mysqli_fetch_assoc($brandQuery)): ?>
-						<option value="<?=$bresult['id'];?>" <?=(($brand == $bresult['id'])?' selected':'');?> >
-							<?=$bresult['brand'];?>
-						</option>
-					<?php endwhile; ?>
-				</select>
-			</div>
-			<div class="form-group col-md-3">
-				<label for="parent">Parent Category*:</label>
-				<select name="parent" id="parent" class="form-control">
-					<option value="" <?=(($parent == '')?' selected':'');?>></option>
-					<?php while($p = mysqli_fetch_assoc($parentQuery)): ?>
-						<option value="<?=$p['id'];?>" <?=(($parent == $p['id'])?' selected':'');?>>
-							<?=$p['category'];?>
-						</option>
-					<?php endwhile; ?>
-				</select>
-			</div>
-			<div class="form-group col-md-3">
-				<label for="child">Child Category*:</label>
-				<select name="child" id="child" class="form-control">
 
-				</select>
-			</div>
-			<div class="form-group col-md-3">
-				<label for="price">Price*:</label>
-				<input type="text" id="price" name="price" class="form-control" value="<?=($price);?>">
-			</div>
-			<div class="form-group col-md-3">
-				<label for="list_price">List Price:</label>
-				<input type="text" id="list_price" name="list_price" class="form-control" value="<?=$list_price;?>">
-			</div>
-			<div class="form-group col-md-3">
-				<label>Quantity & Sizes*:</label>
-				<button class=" form-control btn btn-default" onclick="jQuery('#sizesModal').modal('toggle');return false;">Quantity & Sizes</button>
-			</div>
-			<div class="form-group col-md-3">
-				<label for="sizes">Sizes & Qty preview</label>
-				<input type="text" name="sizes" id="sizes" class="form-control" value="<?=$sizes;?>" readonly>
-			</div>
+		if(!empty($_FILES)){
+			var_dump($_FILES);
+			$photo = $_FILES['photo'];
+			$name = $photo['name'];
+			$nameArray = explode('.',$name);
+			$fileName = $nameArray[0];
+			$fileExt = $nameArray[1];
+			$mime = explode('/',$photo['type']);
+			$mimeType = $mime[0];
+			$mimeExt = $mime[1];
+			$tmpLoc = $photo['tmp_name'];
+			$fileSize = $photo['size'];
+			$allowed = array('png','jpg','jpeg','gif','PNG');
+			$uploadName = md5(microtime()).'.'.$fileExt;
+			$uploadPath = BASEURL.'/images/products/'.$uploadName;
+			$dbpath = '/tutorial/images/products/'.$uploadName;
+			if($mimeType != 'image'){
+				$errors[] = 'The file must be an image.';
+			}
+			if(!in_array($fileExt, $allowed)){
+				$errors[] = 'The file extension must be a png, jpg, jpeg, or gif.';
+			}
+			if($fileSize > 25000000){
+				$errors[] = 'The files size must be under 25 MB.';
+			}
+			if($fileExt != $mimeExt && ($mimeExt == 'jpeg' && $fileExt != 'jgp')){
+				$errors[] = 'File extension does not match the file.';
+			}
+		}
+		if(!empty($errors)){
+			echo display_errors($errors);
+		}else{
+				//upload file and insert into database
+			move_uploaded_file($tmpLoc, $uploadPath);
+			$insertSql = "INSERT INTO products (title,price,list_price,brand,categories,sizes,image,description) VALUES('$title','$price','$list_price', '$brand','$category', '$sizes','$dbpath', '$description')";
 
-			<div class="form-group col-md-6">
+			if(isset($_GET['edit'])){
+				
+				$insertSql = "UPDATE products SET title = '$title', price = '$price', list_price = '$list_price',brand = '$brand', categories = '$category',sizes = '$sizes', image = '$dbpath', description = '$description' WHERE id='$edit_id'";
 
-				<?php if($saved_image != ''): ?>
-					<div class="saved_image">
-						<img src="<?=$saved_image;?>" alt="saved image" />
-						<br>
-						<a href="products.php?delete_image=1&edit=<?=$edit_id;?>" class="text-danger">Delete Image</a>
+				
+			}
+			var_dump($insertSql);
+			$db -> query($insertSql);
+			header('Location: products.php');
+		}
+	}
+	?>
+	<h2 class="text-center"><?=((isset($_GET['edit']))?'Edit ':'Add A New ');?>Product</h2>
+	<hr>
+	<form action="products.php?<?=((isset($_GET['edit']))?'edit='.$edit_id:'add=1');?>" method="POST" enctype="multipart/form-data">
+		<div class="form-group col-md-3">
+			<label for="title">Title*:</label>
+			<input class="form-control" type="text" name="title" id="title" value="<?=$title;?>">
+		</div>
+		<div class="form-group col-md-3">
+			<label for="brand">brand*:</label>
+			<select class="form-control" id="brand" name="brand">
+				<option value="" <?=(($brand == '')?' selected':'');?>></option>
+				<?php while($bresult = mysqli_fetch_assoc($brandQuery)): ?>
+					<option value="<?=$bresult['id'];?>" <?=(($brand == $bresult['id'])?' selected':'');?> >
+						<?=$bresult['brand'];?>
+					</option>
+				<?php endwhile; ?>
+			</select>
+		</div>
+		<div class="form-group col-md-3">
+			<label for="parent">Parent Category*:</label>
+			<select name="parent" id="parent" class="form-control">
+				<option value="" <?=(($parent == '')?' selected':'');?>></option>
+				<?php while($p = mysqli_fetch_assoc($parentQuery)): ?>
+					<option value="<?=$p['id'];?>" <?=(($parent == $p['id'])?' selected':'');?>>
+						<?=$p['category'];?>
+					</option>
+				<?php endwhile; ?>
+			</select>
+		</div>
+		<div class="form-group col-md-3">
+			<label for="child">Child Category*:</label>
+			<select name="child" id="child" class="form-control">
+
+			</select>
+		</div>
+		<div class="form-group col-md-3">
+			<label for="price">Price*:</label>
+			<input type="text" id="price" name="price" class="form-control" value="<?=($price);?>">
+		</div>
+		<div class="form-group col-md-3">
+			<label for="list_price">List Price:</label>
+			<input type="text" id="list_price" name="list_price" class="form-control" value="<?=$list_price;?>">
+		</div>
+		<div class="form-group col-md-3">
+			<label>Quantity & Sizes*:</label>
+			<button class=" form-control btn btn-default" onclick="jQuery('#sizesModal').modal('toggle');return false;">Quantity & Sizes</button>
+		</div>
+		<div class="form-group col-md-3">
+			<label for="sizes">Sizes & Qty preview</label>
+			<input type="text" name="sizes" id="sizes" class="form-control" value="<?=$sizes;?>" readonly>
+		</div>
+
+		<div class="form-group col-md-6">
+			<?php if($saved_image != ''): ?>
+				<div class="saved_image">
+					<img src="<?=$saved_image;?>" alt="saved image" />
+					<br>
+					<a href="products.php?delete_image=1&edit=<?=$edit_id;?>" class="text-danger">Delete Image</a>
+				</div>
+			<?php else: ?>
+				<label for="photo">Product Photo</label>
+				<input type="file" class="form-control" name="photo" id="photo">
+			<?php endif; ?>
+		</div>
+		<div class="form-group col-md-6">
+			<label for="description">Description:</label>
+			<textarea name="description" id="description"   rows="6" class="form-control" placeholder="Describe  here..."><?=$description;?></textarea>
+		</div>
+		<div class="form-group  pull-right">
+			<a href="products.php" class="btn btn-default">Cancel</a>
+			<input type="submit" value="<?=((isset($_GET['edit']))?'Edit':'Add');?> Product" class=" btn btn-success  ">
+		</div>
+		<div class="clearfix"></div>
+
+	</form>
+
+	<!-- Modal -->
+	<div class="modal fade" id="sizesModal" tabindex="-1" role="dialog" aria-labelledby="sizesModalLabel">
+		<div class="modal-dialog modal-lg"  >
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title" id="sizesModalLabel">Size & Quantity </h4>
+				</div>
+				<div class="modal-body">
+					<div class="container-fluid">
+						<?php for($i =1; $i <= 12;$i++):?>
+							<div class="form-group col-md-4">
+								<lable for="size<?=$i;?>">Size:</lable>
+								<input type="text" name="size<?=$i;?>" id="size<?=$i;?>" value="<?=((!empty($sArray[$i-1]))?$sArray[$i-1]:'');?>" class="form-control">
+							</div>
+							<div class="form-group col-md-2">
+								<lable for="qty<?=$i;?>">Quantify:</lable>
+								<input type="number" name="qty<?=$i;?>" id="qty<?=$i;?>" value="<?=((!empty($qArray[$i-1]))?$qArray[$i-1]:'');?>" min="0" class="form-control">
+							</div>
+
+						<?php endfor; ?>
 					</div>
-				<?php else: ?>
-					<label for="photo">Product Photo</label>
-					<input type="file" class="form-control" name="photo" id="photo">
-				<?php endif; ?>
-
-			</div>
-			<div class="form-group col-md-6">
-				<label for="description">Description:</label>
-				<textarea name="description" id="description"   rows="6" class="form-control" placeholder="Describe  here..."><?=$description;?></textarea>
-			</div>
-			<div class="form-group  pull-right">
-				<a href="products.php" class="btn btn-default">Cancel</a>
-				<input type="submit" value="<?=((isset($_GET['edit']))?'Edit':'Add');?> Product" class=" btn btn-success  ">
-			</div>
-			<div class="clearfix"></div>
-
-		</form>
-
-		<!-- Modal -->
-		<div class="modal fade" id="sizesModal" tabindex="-1" role="dialog" aria-labelledby="sizesModalLabel">
-			<div class="modal-dialog modal-lg"  >
-				<div class="modal-content">
-					<div class="modal-header">
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title" id="sizesModalLabel">Size & Quantity </h4>
-					</div>
-					<div class="modal-body">
-						<div class="container-fluid">
-							<?php for($i =1; $i <= 12;$i++):?>
-								<div class="form-group col-md-4">
-									<lable for="size<?=$i;?>">Size:</lable>
-									<input type="text" name="size<?=$i;?>" id="size<?=$i;?>" value="<?=((!empty($sArray[$i-1]))?$sArray[$i-1]:'');?>" class="form-control">
-								</div>
-								<div class="form-group col-md-2">
-									<lable for="qty<?=$i;?>">Quantify:</lable>
-									<input type="number" name="qty<?=$i;?>" id="qty<?=$i;?>" value="<?=((!empty($qArray[$i-1]))?$qArray[$i-1]:'');?>" min="0" class="form-control">
-								</div>
-
-							<?php endfor; ?>
-						</div>
-					</div>
-					<div class="modal-footer">
-						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-						<button type="button" class="btn btn-primary" onclick="updateSizes();jQuery('#sizesModal').modal('toggle');return false;">Save changes</button>
-					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					<button type="button" class="btn btn-primary" onclick="updateSizes();jQuery('#sizesModal').modal('toggle');return false;">Save changes</button>
 				</div>
 			</div>
 		</div>
+	</div>
 
-		<?php }else{
-
-
-			$sql = "SELECT * FROM products WHERE deleted = 0";
-			$presults = $db -> query($sql);
-			if(isset($_GET['featured'])) {
-				$id = (int)$_GET['id'];
-				$featured = (int)$_GET['featured'];
-				$featuredSql = "UPDATE products SET featured = $featured WHERE id=$id ";
-				$db -> query($featuredSql);
-				header('Location: products.php');
-			}
-			?>
-
-			<h2 class="text-center">Products</h2>
-			<a href="products.php?add=1" class="btn btn-success pull-right" id="add-product-btn">
-				Add Product
-			</a>
-			<div class="clearfix"></div>
-			<hr>
-			<table class="table table-bordered table-condensed table-striped">
-				<thead>
-					<th></th>
-					<th>Product</th>
-					<th>Price</th>
-					<th>Categories</th>
-					<th>Featured</th>
-					<th>Sold</th>
-				</thead>
-				<tbody>
-					<?php while($product = mysqli_fetch_assoc($presults)): 
-					$childID = $product['categories'];
-					$catSql = "SELECT * FROM categories WHERE id='$childID' ";
-					$result = $db -> query($catSql);
-					$child = mysqli_fetch_assoc($result);
-					$parentID = $child['parent'];
-					$pSql = "SELECT * FROM categories WHERE id= '$parentID' ";
-					$presult = $db -> query($pSql);
-					$parent = mysqli_fetch_assoc($presult);
-					$category = $parent['category'].'~'.$child['category'];
-					?>
-					<tr>
-						<td>
-							<a href="products.php?edit=<?=$product['id']?>" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-pencil"></span></a>
-							<a href="products.php?delete=<?=$product['id']?>" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-remove"></span></a>
-						</td>
-						<td><?=$product['title'];?></td>
-						<td><?=money($product['price']);?></td>
-						<td><?=$category;?></td>
-						<td>
-							<a href="products.php?featured=<?=(($product['featured'] == 0)?'1':'0');?>&id=<?=$product['id'];?>" class="btn btn-xs btn-default ">
-								<span class="glyphicon glyphicon-<?=(($product['featured'] == 1)?'minus':'plus');?>">
-								</span>
-							</a>
-							&nbsp <?=(($product['featured'] == 1)?'Featured product':'');?>
-						</td>
-						<td></td>
-					</tr>
-				<?php endwhile; ?>
-			</tbody>
-		</table>
+	<?php }else{
 
 
-
-		<?php }
-		include 'includes/footer.php';
+		$sql = "SELECT * FROM products WHERE deleted = 0";
+		$presults = $db -> query($sql);
+		if(isset($_GET['featured'])) {
+			$id = (int)$_GET['id'];
+			$featured = (int)$_GET['featured'];
+			$featuredSql = "UPDATE products SET featured = $featured WHERE id=$id ";
+			$db -> query($featuredSql);
+			header('Location: products.php');
+		}
 		?>
 
-		<script>
+		<h2 class="text-center">Products</h2>
+		<a href="products.php?add=1" class="btn btn-success pull-right" id="add-product-btn">
+			Add Product
+		</a>
+		<div class="clearfix"></div>
+		<hr>
+		<table class="table table-bordered table-condensed table-striped">
+			<thead>
+				<th></th>
+				<th>Product</th>
+				<th>Price</th>
+				<th>Categories</th>
+				<th>Featured</th>
+				<th>Sold</th>
+			</thead>
+			<tbody>
+				<?php while($product = mysqli_fetch_assoc($presults)): 
+				$childID = $product['categories'];
+				$catSql = "SELECT * FROM categories WHERE id='$childID' ";
+				$result = $db -> query($catSql);
+				$child = mysqli_fetch_assoc($result);
+				$parentID = $child['parent'];
+				$pSql = "SELECT * FROM categories WHERE id= '$parentID' ";
+				$presult = $db -> query($pSql);
+				$parent = mysqli_fetch_assoc($presult);
+				$category = $parent['category'].'~'.$child['category'];
+				?>
+				<tr>
+					<td>
+						<a href="products.php?edit=<?=$product['id']?>" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-pencil"></span></a>
+						<a href="products.php?delete=<?=$product['id']?>" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-remove"></span></a>
+					</td>
+					<td><?=$product['title'];?></td>
+					<td><?=money($product['price']);?></td>
+					<td><?=$category;?></td>
+					<td>
+						<a href="products.php?featured=<?=(($product['featured'] == 0)?'1':'0');?>&id=<?=$product['id'];?>" class="btn btn-xs btn-default ">
+							<span class="glyphicon glyphicon-<?=(($product['featured'] == 1)?'minus':'plus');?>">
+							</span>
+						</a>
+						&nbsp <?=(($product['featured'] == 1)?'Featured product':'');?>
+					</td>
+					<td></td>
+				</tr>
+			<?php endwhile; ?>
+		</tbody>
+	</table>
 
-			jQuery('document').ready(function(){
-				get_child_options('<?=$category;?>')
-			});
-		</script>
+
+
+	<?php }
+	include 'includes/footer.php';
+	?>
+
+	<script>
+
+		jQuery('document').ready(function(){
+			get_child_options('<?=$category;?>')
+		});
+	</script>

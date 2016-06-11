a46fb6dea74370431bf2341a840e700d529b9091
Moss Green <gufeifei@outlook.com>
2016-06-11 21:09:57 +1200

    tested. finish validation section in adding users page

--- admin/users.php
+++ admin/users.php
@@ -23,17 +23,43 @@ if(isset($_GET['add'])){
 	$permissions=((isset($_POST['permissions']))?sanitize($_POST['permissions']):'');
 	$errors = array();
 	if ($_POST) {
+
+		$emailQuery = $db -> query("SELECT * FROM users WHERE email = '$email'");
+		$emailCount = mysqli_num_rows($emailQuery);
+
+		if($emailCount != 0){
+			$errors[]='That email already exists in our database.';
+		}
+
 		$required = array('name','email','password','confirm','permissions');
 		foreach ($required as $f) {
 			if(empty($_POST[$f])){
-				$errors[] = 'You must fill out all fields';
+				$errors[] = 'You must fill out all fields.';
 				break;
 			}
 		}
+
+		if(strlen($password) < 6){
+			$errors[] = 'Your password must be at 6 characters.';
+		}
+
+		if($password != $confirm){
+			$errors[]='Your passwords do not match.';
+		}
+
+		if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+			$errors[] = 'You must enter a valid email.';
+		}
+
+
 		if(!empty($errors)){
 			echo display_errors($errors);
 		}else{
 			//add user to datebase
+			$hashed = password_hash($password,PASSWORD_DEFAULT);
+			$db -> query("INSERT INTO users (full_name, email, password,permissions) VALUES('$name','$email','$hashed','$permissions')");
+			$_SESSION['success_flash'] = 'User has been added!';
+			header('Location: users.php');
 		}
 	}
 	?>
@@ -45,7 +71,7 @@ if(isset($_GET['add'])){
 		</div>
 		<div class="form-group col-md-6">
 			<label for="email">Email:</label>
-			<input type="text" id="email" class="form-control" name="email" value="<?=$email;?>">
+			<input type="email" id="email" class="form-control" name="email" value="<?=$email;?>">
 		</div>
 		<div class="form-group col-md-6">
 			<label for="password">Password:</label>
@@ -57,7 +83,7 @@ if(isset($_GET['add'])){
 		</div>
 		<div class="form-group col-md-6">
 			<label for="permissions">Permissions:</label>
-			<select class="form-control">
+			<select class="form-control" name="permissions">
 				<option value="" <?=(($permissions == '')?' selected':'');?>></option>
 				<option value="editor" <?=(($permissions == 'editor')?' selected':'');?>>Editor</option>
 				<option value="admin,editor" <?=(($permissions == 'admin,editor')?' selected':'');?>>Admin</option>
@@ -98,7 +124,7 @@ if(isset($_GET['add'])){
 					<td><?=$user['full_name'];?></td>
 					<td><?=$user['email'];?></td>
 					<td><?=pretty_date($user['join_date']);?></td>
-					<td><?=pretty_date($user['last_login']);?></td>
+					<td><?=(($user['last_login'] == '0000-00-00 00:00:00')?'N/A':pretty_date($user['last_login']));?></td>
 					<td><?=$user['permissions'];?></td>
 				</tr>
 			<?php endwhile; ?>

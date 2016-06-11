b6934ff4bd1bee24bf48c17dfab9c9a5f955151a
Moss Green <gufeifei@outlook.com>
2016-06-11 12:13:28 +1200

    tested. styling with login page

--- admin/login.php
+++ admin/login.php
@@ -1,12 +1,33 @@
 <?php 
-print_r($_REQUEST);
 require_once $_SERVER['DOCUMENT_ROOT'].'/tutorial/core/init.php';;
 include 'includes/head.php';
-$password = 'password';
-$hashed = password_hash($password, PASSWORD_DEFAULT);
-echo $hashed;
 
 ?>
+<style>
+body{
+	background-image: url("/tutorial/images/headerlogo/background.png");
+	background-size: 100vw 100vh;
+}
+</style>
+<div id="login-form">
+	<div></div>
+	<h2 class="text-center">Login</h2><hr>
+	<form action="login.php" method="POST">
+		<div class="form-group">
+			<label for="email">Email: </label>
+			<input type="email" name="email" id="email" class="form-control" value="<?=$email;?>">
+		</div>
+		<div class="form-group">
+			<label for="password">Password: </label>
+			<input type="password" name="password" id="password" class="form-control" value="<?=$password;?>">
+		</div>
+		<div class="form-group">
+			<input type="submit" value="Login" class="btn btn-primary">
+		</div>
+
+	</form>
+	<p class="text-right"><a href="/tutorial/index.php" alt="home">Visite Site</a></p>
+</div>
 
 
 <?php include 'includes/footer.php'; ?>

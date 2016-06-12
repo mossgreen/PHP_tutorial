b7087b062e8925367943ed8f203228d6b40a26aa
Moss Green <gufeifei@outlook.com>
2016-06-12 16:35:24 +1200

    tested. can add items to same cookie id

--- admin/parsers/add_cart.php
+++ admin/parsers/add_cart.php
@@ -20,8 +20,30 @@ $_SESSION['success_flash'] = $product['title'].' was added to your cart.';
 
 //check to see if the cart cookie exists
 if($cart_id != ''){
+	$cartQ = $db -> query("SELECT * FROM cart WHERE id='{$cart_id}'");
+	$cart = mysqli_fetch_assoc($cartQ);
+	$previous_items = json_decode($cart['items'], true);
+	$item_match = 0;
+	$new_item = array();
+	foreach ($previous_items as $pitem) {
+		if($item[0]['id'] == $pitem['id'] && $item[0]['size'] == $pitem['size']){
+			$pitem['quantity'] = $pitem['quantity'] + $item[0]['quantity'];
+			if($pitem['quantity'] > $available){
+				$pitem['quanlity'] = $available;
+			}
+			$item_match = 1;
+		}
+		$new_items[] = $pitem;
+	}
+	if($item_match != 1){
+		$new_items = array_merge($item, $previous_items);
+	}
 
-
+	$items_json = json_encode($new_items);
+	$cart_expire= date("Y-m-d H:i:s", strtotime("+30 days"));
+	$db -> query("UPDATE cart SET items = '{$items_json}', expire_date = '{$$cart_expire}' WHERE id='{$cart_id}'");
+	setcookie(CART_COOKIE,'',1,"/",$domain, false);
+	setcookie(CART_COOKIE,$cart_id, CART_COOKIE_EXPIRE,'/',$domain, false);
 
 }else{
 	//add the car to database and set cookie

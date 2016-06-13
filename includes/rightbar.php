cd114ce991494a72f8ec3d7e1dbff991ac331c7b
Moss Green <gufeifei@outlook.com>
2016-06-13 16:17:47 +1200

    added two widgets

--- includes/rightbar.php
+++ includes/rightbar.php
@@ -1,4 +1,9 @@
 		<!-- right side bar -->
 		<div class="col-md-2">
 			Right side bar
+
+			<?php 
+			include 'widgets/cart.php';
+			include 'widgets/recent.php';
+			 ?>
 		</div>
--- includes/widgets/cart.php
+++ includes/widgets/cart.php
@@ -1,1 +1,44 @@
-<h3 class="text-center">Shopping Cart</h3>
+<h3 class="text-center" >Shopping Cart</h3>
+<div>
+	<?php if(empty($cart_id)): ?>
+		
+		<p>Your Shopping Cart is empty.</p>
+	<?php else:
+	$cartQ = $db -> query("SELECT * FROM cart WHERE id='{$cart_id}'");
+	$results = mysqli_fetch_assoc($cartQ);
+	$items = json_decode($results['items'],true);
+	$i = 1;
+	$sub_total = 0;
+	?>
+
+	<table class="table table-condensed " id="cart_widget">
+		<thead></thead>
+		<tbody>
+			<?php foreach($items as $item):
+			$productQ = $db -> query("SELECT * FROM products WHERE id='{$item['id']}'");
+			$product = mysqli_fetch_assoc($productQ);
+			?>
+			<tr>
+				<td><?=$item['quantity'];?></td>
+				<td><?=substr($product['title'],0,15)?></td>
+				<td><?=money($item['quantity']*$product['price']);?></td>
+			</tr>
+			<?php 
+			$sub_total += ($item['quantity']*$product['price']);
+
+			endforeach; ?>
+
+			<tr>
+				<td></td>
+				<td>Sub Total</td>
+				<td><?=money($sub_total);?></td>
+			</tr>
+
+		</tbody>
+	</table>
+
+	<a href="cart.php" class="btn btn-xs btn-primary pull-right">view Cart</a>
+	<div class="clearfix"></div>
+<?php endif; ?>
+
+</div>

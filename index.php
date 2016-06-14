ab7038247f77aab94e9b1a374699e770b9d3ef93
Moss Green <gufeifei@outlook.com>
2016-06-14 22:36:36 +1200

    bug fixed with product pic on index page

--- index.php
+++ index.php
@@ -17,7 +17,8 @@ $featured = $db -> query($sql);
 			
 			<div class="col-md-3 text-center">
 				<h4><?=$product['title']; ?></h4>
-				<img src="<?=$product['image']; ?>" alt="<?=$product['title']; ?>" class="img-thumb" />
+				<?php $photos=explode(',',$product['image']) ?>
+				<img src="<?=$photos[0]; ?>" alt="<?=$product['title']; ?>" class="img-thumb" />
 				<p class="list-price text-danger">List Price: <s>$<?=$product['list_price']; ?></s></p>
 				<p class="price">Our Price:$<?=$product['price']; ?></p>
 				

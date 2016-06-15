357f33e96e406e5bdaa03e4487b961069b4a90ed
Moss Green <gufeifei@outlook.com>
2016-06-15 20:19:42 +1200

    added logo

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # PHP_tutorial
 
-this project is donw by following a youtube video which names: eCommerce with PHP. address is here: https://www.youtube.com/playlist?list=PLFPkAJFH7I0mitTSKDaoxwfLLf-wNNnVS
+this project is done by following a youtube video which names: eCommerce with PHP. address is here: https://www.youtube.com/playlist?list=PLFPkAJFH7I0mitTSKDaoxwfLLf-wNNnVS
 
 infact, i will modify and refine this project. it's diveded into two versions: master is my project and will on line someday. branch 'forAssignment' is for my assignment. I will modify some function to make it fit my assginment's requirement.
 
--- includes/navigation.php
+++ includes/navigation.php
@@ -4,30 +4,44 @@ $pquery = $db -> query($sql);
 ?>
 
 
-<nav class="navbar navbar-default navbar-fixed-top">
-	<div class="contianer">
-		<a href="index.php" class="navbar-brand">moss's shop</a>
-		<ul class="nav navbar-nav">
-			<?php while($parent = mysqli_fetch_assoc($pquery)): ?>
-				<?php 
-				$parent_id = $parent['id']; 
-				$sql2 = "SELECT * FROM categories WHERE parent = '$parent_id'";
-				$cquery = $db -> query($sql2);
-				?>
+<nav class="navbar navbar-default navbar-fixed-top" >
+	<div class="contianer-fluid">
+		<div class="navbar-header" >
 
-				<!-- menu items -->
-				<li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo $parent['category']; ?><span class="caret"></span></a>
-					<ul class="dropdown-menu" role="menu">
-						<?php while($child = mysqli_fetch_assoc($cquery)): ?>
-							<li><a href="category.php?cat=<?=$child['id'];?>"><?php echo $child['category']; ?></a></li>
-						<?php endwhile; ?>
-					</ul>
-				</li>
-				
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-container">
+				<span class="sr-only">Show the hide the navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="index.php">
+				<img src="../tutorial/images/headerlogo/logo.jpg" id="mylogo">
+			</a>
+			
+		</div>
+		<div class="collapse navbar-collapse" id="navbar-container">
+			<ul class="nav navbar-nav" >
+				<?php while($parent = mysqli_fetch_assoc($pquery)): ?>
+					<?php 
+					$parent_id = $parent['id']; 
+					$sql2 = "SELECT * FROM categories WHERE parent = '$parent_id'";
+					$cquery = $db -> query($sql2);
+					?>
 
-			<?php endwhile; ?>
-			<li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span> My Cart</a></li>
-		</ul>
+					<!-- menu items -->
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo $parent['category']; ?><span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<?php while($child = mysqli_fetch_assoc($cquery)): ?>
+								<li><a href="category.php?cat=<?=$child['id'];?>"><?php echo $child['category']; ?></a></li>
+							<?php endwhile; ?>
+						</ul>
+					</li>
+
+
+				<?php endwhile; ?>
+				<li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span> My Cart</a></li>
+			</ul>
+		</div>
 	</div>
 </nav>
